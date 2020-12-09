1bf7cc7217ddbfbc9e2e63a65f564b75ddfd72e5
LambertPierre <pierrelambert2002@gmail.com>
2020-12-09 16:49:20 +0100

    Update exam_python_9_12.py

--- exam_python_9_12.py
+++ exam_python_9_12.py
@@ -1,28 +1,59 @@
+
 from colorama import init
 init()
-#from colorama import Fore, Back, Style
+from colorama import Fore, Back, Style
 #print(Fore.RED + 'some red text', end=" ")
 #print(Back.GREEN + 'and with a green background')
 #print(Style.DIM + 'and in dim text')
 #print(Style.RESET_ALL)
 #print('back to normal now')
 
-Reponse1=["n","a","c","r","e"]
-Reponse2=["l","i","t","r","e"]
-Reponse3=["t","r","o","n","c"]
-Reponse4=["h","a","c","h","e"]
+ligne=["_","_","_","_","_","_"]
 
-ligne1=["_","_","_","_","_","_"]
-ligne2=["_","_","_","_","_","_"]
-ligne3=["_","_","_","_","_","_"]
-ligne4=["_","_","_","_","_","_"]
+essais = 0
+Victoire=False
 
-def afficheGrille():
-        print (ligne1)
-        print (ligne2)
-        print (ligne3)
-        print (ligne4)
+def afficheLigne():
+    print (ligne)
+    
+def proposerMot ():
+    LigneEssais()
+    Reponse1="jungle"
+    Reponse2="fluide"
+    Reponse3="hyrule"
+    Reponse4="profit"
+    motPropose = input("Veillez choisir une réponse...")
+    for i in range(0,6):
+        if (motPropose[i] == Reponse1[i]):
+            print (Fore.RED + motPropose[i], end=" ")
+            print(Style.RESET_ALL)
+            print ("Bonne lettre et bon emplacement!")
+            ligneReponse[i]= motPropose[i]
+            position = i+1
+            print ("la position est:",position)
+        else :
+            for j in range(0,6):
+                if motPropose[i] == Reponse1[j]:
+                    print (Fore.YELLOW + motPropose[i], end=" ")
+                    print(Style.RESET_ALL)
+                    print ("Bonne lettre, mais mauvaise emplacement")
 
-afficheGrille()
+def testVictoire():
+    bonneLettre = 0
+    for i in range(0,5):
+        if (ligne1[i]==Reponse1[i]):
+            bonneLettre+=1
+    if (bonneLettre==6):
+        Victoire=True
+        print("Bravo ! Vous avez trouvé le bon mot !")
 
+
+
+print ("Bienvenue au Motus Python !")
+while (essais<8 or Victoire==False):
+    afficheGrille()
+    proposerMot()
+    testVictoire()
+    essais+=1
+print ("Vous avez plus d'autre essaie :'(")
 input()
